390bf350897ca01ec591e3d9c5d75e9e8ad8cc32
Gray Olson <gray@grayolson.com>
2017-07-18 20:26:54 -0400

    update generate json

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+
+	"io/ioutil"
 
 	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
 )
+
+// Address type for json encoding
+type Address struct {
+	Seed modules.Seed     `json:"seed"`
+	Esk  crypto.SecretKey `json:"esk"`
+	Epk  crypto.PublicKey `json:"epk"`
+	Add  types.UnlockHash `json:"address"`
+}
+
+// TestVectors is the form of overall json output
+type TestVectors struct {
+	SeedStr         string       `json:"seedStr"`
+	SeedBytes       []byte       `json:"seedBytes"`
+	SeedEng         string       `json:"seedEng"`
+	SeedHashed      modules.Seed `json:"seedHashed"`
+	DerivedAdresses []Address    `json:"derivedAddresses"`
+}
 
 func getAddress(seed modules.Seed) (esk crypto.SecretKey, epk crypto.PublicKey, add types.UnlockHash) {
 	esk, epk = crypto.GenerateKeyPairDeterministic(seed)
@@ -18,15 +39,42 @@ func getAddress(seed modules.Seed) (esk crypto.SecretKey, epk crypto.PublicKey, 
 }
 
 func main() {
-	var seed modules.Seed
-	seedStr := "paper hard ripple dwarf wise ski salute middle crouch stuff broom paper"
-	seedBytes := []byte(seedStr)
-	seed = modules.Seed(crypto.HashBytes(seedBytes))
-	esk, epk, add := getAddress(seed)
-	finalSeedStr, _ := modules.SeedToString(seed, "english")
-	fmt.Println("Raw Seed:", seed)
-	fmt.Println("English Seed:", finalSeedStr)
-	fmt.Println("ESK:", esk)
-	fmt.Println("EPK:", epk)
-	fmt.Println("Address:", add)
+	var verbose bool
+	flag.BoolVar(&verbose, "v", false, "verbose?")
+	flag.Parse()
+	var testVectors TestVectors
+	testVectors.SeedStr = "paper hard ripple dwarf wise ski salute middle crouch stuff broom paper"
+	testVectors.SeedBytes = []byte(testVectors.SeedStr)
+	testVectors.SeedHashed = modules.Seed(crypto.HashBytes(testVectors.SeedBytes))
+	testVectors.SeedEng, _ = modules.SeedToString(testVectors.SeedHashed, "english")
+	if verbose {
+		fmt.Println("::: ::: Master seed")
+		fmt.Println("::: String:", testVectors.SeedStr)
+		fmt.Println("::: Bytes:", testVectors.SeedBytes)
+		fmt.Println("::: Hashed:", testVectors.SeedHashed)
+		fmt.Println("::: English:", testVectors.SeedEng)
+		fmt.Println(":::")
+		fmt.Println(":::")
+	}
+	var addresses []Address
+	for i := uint64(0); i < 20; i++ {
+		s := modules.Seed(crypto.HashAll(testVectors.SeedHashed, i))
+		esk, epk, add := getAddress(s)
+		addresses = append(addresses, Address{Seed: s, Esk: esk, Epk: epk, Add: add})
+		if verbose {
+			fmt.Println("::: ::: Address", i)
+			fmt.Println("::: Hashed Seed:", s)
+			fmt.Println("::: ESK:", esk)
+			fmt.Println("::: EPK:", epk)
+			fmt.Println("::: Address:", add)
+			fmt.Println(":::")
+			fmt.Println(":::")
+		}
+	}
+	testVectors.DerivedAdresses = addresses
+	j, err := json.Marshal(testVectors)
+	if err != nil {
+		fmt.Println(err)
+	}
+	ioutil.WriteFile("testVectors.json", j, 0644)
 }
